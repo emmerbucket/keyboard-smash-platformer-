1a7b09835c492567ba6a5094f4351aa654547475
Ola <16321@kalsbeek.nl>
2025-12-01 13:58:59 +0100

    paar veranderingen

--- pages/main_platformer.py
+++ pages/main_platformer.py
@@ -1,8 +1,9 @@
-import arcade
+import pygame
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Keyboard Smash"
+
 
 class MyGame(arcade.Window):
 
@@ -26,3 +27,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class Player():
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.image = pygame.Surface([15, 15])
+        self.image.fill(WHITE)
+
+        self.change_x = 0
+        self.change_y = 0
+    
+    def changespeed(self, x, y):
+        self.change_x += x
+        self.change_y += y
+    
+    def update(self):
+        self.rect.x += self.change_x
