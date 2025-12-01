9432ee45041956f4285e15d27bdfe43623ad1ca5
emmerbucket <16748@kalsbeek.nl>
2025-12-01 14:20:30 +0100

    tabbed class player

--- pages/main_platformer.py
+++ pages/main_platformer.py
@@ -17,19 +17,19 @@ while True:
     pygame.display.update()
     clock.tick(24)
 
-class Player():
-    def __init__(self, x, y):
-        super().__init__()
+    class Player():
+        def __init__(self, x, y):
+            super().__init__()
 
-        self.image = pygame.Surface([15, 15])
-        self.image.fill(WHITE)
+            self.image = pygame.Surface([15, 15])
+            self.image.fill(WHITE)
 
-        self.change_x = 0
-        self.change_y = 0
-    
-    def changespeed(self, x, y):
-        self.change_x += x
-        self.change_y += y
-    
-    def update(self):
-        self.rect.x += self.change_x
+            self.change_x = 0
+            self.change_y = 0
+        
+        def changespeed(self, x, y):
+            self.change_x += x
+            self.change_y += y
+        
+        def update(self):
+            self.rect.x += self.change_x
