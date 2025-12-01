099def110c963a78876ece4dbb0e016e17f72408
Ola <16321@kalsbeek.nl>
2025-12-01 14:05:40 +0100

    window weg

--- pages/main_platformer.py
+++ pages/main_platformer.py
@@ -5,41 +5,6 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Keyboard Smash"
 
 
-class MyGame(arcade.Window):
-
-    def __init__(self):
-
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        arcade.set_background_color(arcade.csscolor.BLACK)
-
-        arcade.PhysicsEnginePlatformer
-
-        self.background = []
-
-        self.walls = []
-
-        self.player = []
-
-        self.physics_engine = []
-
-        self.level = []
-
-    def setup(self):
-        pass
-
-    def on_draw(self):
-        self.clear()
-
-def main():
-    window = MyGame()
-    window.setup()
-    arcade.run()
-
-
-if __name__ == "__main__":
-    main()
-
 class Player():
     def __init__(self, x, y):
         super().__init__()
