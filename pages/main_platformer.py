d01ed11b345ec959e41d2ac2a68ccc92d78b9c18
emmerbucket <16748@kalsbeek.nl>
2025-12-12 14:34:46 +0100

    made new level making thingy but movement's fucked

--- pages/main_platformer.py
+++ pages/main_platformer.py
@@ -2,14 +2,14 @@ import pygame
 import os
 
 backgrounds = []
-bg_files = ['christmasbackground1.png', 'christmasbackground2.png', 'library.png', 'mountains.png', 'orangetree.png']
+bg_files = ['orangetree.png', 'mountains.png', 'snowyhouse.png', 'library.png', 'christmasbackground1.png', 'christmasbackground2.png']
 
 for bg_file in bg_files:
     bg_image = pygame.image.load(os.path.join(os.path.dirname(__file__), '..', 'images', bg_file))
     bg_image = pygame.transform.scale(bg_image, (800, 600))
     backgrounds.append(bg_image)
 
-current_bg_index = 4
+current_bg_index = 0
 walls = []
 player = []
 levelcount = 0
@@ -57,17 +57,56 @@ class Floor():
         self.change_x = 0
         self.change_y = 0
 
+floor = None
 
-floor = Floor()
+class Block():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 32
+        self.height = 25
+        self.image = pygame.Surface([self.width, self.height])
+        floor_img = pygame.image.load(os.path.join(os.path.dirname(__file__), '..', 'images', 'woodblock.png'))
+        self.image = pygame.transform.scale(floor_img, (self.width, self.height))
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+level1 = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+blocklist = []
+for y in range(len(level1)):
+    for x in range(len(level1[y])):
+        if level1[y][x] == 1:
+            blocklist.append(Block(x * 32, y * 32))
 
 def draw():
     window.blit(backgrounds[current_bg_index], (0, 0))
+    for block in blocklist:
+        window.blit(block.image, block.rect)
+    window.blit(player.image, player.rect)
     if floor:
         window.blit(floor.image, floor.rect)
-    window.blit(player.image, player.rect)
 
-vel = 5
-jump = False
+vel = 1
 jumpCount = 0
 jumpMax = 15
 
@@ -79,23 +118,27 @@ while True:
             exit()
 
         if event.type == pygame.KEYDOWN:
-         if event.key == pygame.K_SPACE and not jump:
+         if event.key == pygame.K_SPACE and player.on_ground:
             player.change_y = -15
-            jump = True
-
+            player.on_ground = False
 
     keys = pygame.key.get_pressed()
 
     player.change_x = 0
 
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        player.change_x = -5
+        player.change_x = -1
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        player.change_x = 5
+        player.change_x = 1
     
     player.change_y += 1 
+    if player.change_y > 12:
+        player.change_y = 12
+
+    player.rect.x += player.change_x
+    player.rect.y += player.change_y
     
-    player.update()
+    player.on_ground = False
 
     if floor and player.rect.colliderect(floor.rect):
         if player.change_y > 0:
@@ -105,11 +148,31 @@ while True:
         elif player.change_y < 0:
             player.rect.top = floor.rect.bottom
             player.change_y = 0
-    else:
-        player.on_ground = False
+
+    # horizontal collision
+    for block in blocklist:
+        if player.rect.colliderect(block.rect):
+            if player.change_x > 0:
+                player.rect.right = block.rect.left
+                break
+            elif player.change_x < 0:
+                player.rect.left = block.rect.right
+                break
+
+    # vertical collision
+    for block in blocklist:
+        if player.rect.colliderect(block.rect):
+            if player.change_y > 0:
+                player.rect.bottom = block.rect.top
+                player.change_y = 0
+                player.on_ground = True
+                break
+            elif player.change_y < 0:
+                player.rect.top = block.rect.bottom
+                player.change_y = 0
+                break
 
     draw()
 
     pygame.display.update()
     clock.tick(24)
-
